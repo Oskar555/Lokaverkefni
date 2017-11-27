53952ebc1583caa2dcc4eae40f425b9483fdda3c
Oskar Jonsson <oskarjonsson12@outlook.com>
2017-11-27 10:10:31 +0000

    Fyrsta commit

---
+++ lokaverkefni/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div id="tasks" v-for='task in tasks'>
+    	<h1>{{ tasks.data }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  mounted(){
+  	var self = this;
+
+  	axios.get('http://fjolbraut.org/api/tasks', {
+        params: {
+        	api_token: 'jb5ZdxkzaL8dNs5VVyM1mu2YQCp9p3DIFxU7qcap32KLrqr5spVXGFA20K2e'
+            }
+        })
+  	.then(function(response) {
+  		self.tasks = response.data.results;
+  		console.log(response)
+  	})
+  	.catch(function(error) {
+  		console.log(error);
+  	});
+  },
+  computed: {
+  	
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
