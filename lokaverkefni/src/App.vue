ba888908b2726956eb43f3a7eb29b07891c66296
Oskar Jonsson <oskarjonsson12@outlook.com>
2017-11-28 10:09:41 +0000

    second commit

--- lokaverkefni/src/App.vue
+++ lokaverkefni/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <div id="tasks" v-for='task in tasks'>
-    	<h1>{{ tasks.data }}</h1>
+    	<input type="text" v-model='task'>
+    	<button @click='taskInput'>bæta við</button>
+    <div id="Tasks" v-for='task in tasks'>
+    	<ul>
+    		<li>{{ task.title }}</li>
+    	</ul>
     </div>
   </div>
 </template>
@@ -13,31 +17,46 @@ export default {
   name: 'app',
   data () {
     return {
-      tasks: []
-    }
+      tasks: [],
+      task: ''
+    };
   },
   mounted(){
   	var self = this;
-
   	axios.get('http://fjolbraut.org/api/tasks', {
         params: {
-        	api_token: 'jb5ZdxkzaL8dNs5VVyM1mu2YQCp9p3DIFxU7qcap32KLrqr5spVXGFA20K2e'
-            }
+        	api_token: 'jb5ZdxkzaL8dNs5VVyM1mu2YQCp9p3DIFxU7qcap32KLrqr5spVXGFA20K2e'}
         })
-  	.then(function(response) {
-  		self.tasks = response.data.results;
-  		console.log(response)
-  	})
-  	.catch(function(error) {
-  		console.log(error);
-  	});
-  },
-  computed: {
+  		.then(function(response) {
+  			self.tasks = response.data;
+  			console.log(response)
+  		})
+  		.catch(function(error) {
+  			console.log(error);
+  		});
+
   	
+  },
+  methods: {
+  	taskInput: function(){
+  		axios.post('http://fjolbraut.org/api/tasks?api_token=jb5ZdxkzaL8dNs5VVyM1mu2YQCp9p3DIFxU7qcap32KLrqr5spVXGFA20K2e', {
+            title: this.task
+         })
+         .then(function(response) {
+           this.tasks.push(this.task);
+           this.task = ''
+         })
+         .catch(function(error) {
+            console.log(error);
+         });
+
+
+  		//this.tasks.push(this.task);
+  		//this.task = ''
+  	}
   }
 }
 </script>
 
 <style lang="scss">
-
 </style>
